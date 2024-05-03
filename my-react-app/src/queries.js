0d3a0413218a249a7403a178501dd02675c053d4
Luke Roumy <Luke5r@icloud.com>
2024-05-03 18:39:28 -0400

    Small push

--- my-react-app/src/queries.js
+++ my-react-app/src/queries.js
@@ -1,27 +1,29 @@
 import { gql } from '@apollo/client';
 
 const GET_SPOTIFY_AUTH_URL = gql`
-    query {
-        getSpotifyAuthUrl
-    }
+  query GetSpotifyAuthUrl {
+    getSpotifyAuthUrl
+  }
 `;
+
 const CREATE_USER = gql`
-    createUser {
-        user{
-            _id
-            email
-            authorized
-        }
+  mutation CreateUser($email: String!, $password: String!) {
+    createUser(email: $email, password: $password) {
+      _id
+      email
+      authorized
     }
+  }
 `;
+
 const AUTHORIZE_SPOTIFY = gql`
-    authorizeSpotify {
-        user{
-            _id
-            email
-            authorized
-        }
+    mutation AuthorizeSpotify($id: String!, $code: String!) {
+    authorizeSpotify(_id: $id, code: $code) {
+      _id
+      email
+      authorized
     }
+  }
 `;
 
 const exported = {
