ffd04b23235b100824afe9d50f6daa6ee42cf8d1
Charles Canata <ccanata@stevens.edu>
2024-05-04 11:12:49 -0400

    Set up Cookies

--- my-react-app/src/components/Chart.jsx
+++ my-react-app/src/components/Chart.jsx
@@ -5,8 +5,6 @@ export function Chart(props) {
         <div id="Chart-div">
              {!props.hideInfo && <button className="info-button" onClick={() => {location.href="/chart"}}>i</button>}
             <h1>CHART</h1>
-
-            <button onClick={() => {setCookie('user', "Success", {path: '/'})}}>TEST COOKIES</button>
         </div>
     )
 }
