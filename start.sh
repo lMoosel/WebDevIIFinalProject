dec9ecc869ce600e49b2a94ca33d950add2cfc88
kaikune <alexkai03@live.com>
2024-05-05 23:03:28 -0400

    Changed start.sh to work on Unix

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-fuser -k 5173/tcp   # kills process running on port 5173 (hopefully a Vite server)
+# fuser --kill 5173/tcp # kills process running on port 5173 (hopefully a Vite server)
+lsof -i tcp:5173 | grep -v PID | awk '{print $2}' | xargs kill # To work on unix
 
 cd server
 npm i
