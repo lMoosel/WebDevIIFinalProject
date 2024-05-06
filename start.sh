ee6d357859ecc0923fb0f69a6b1c4dac3e66d624
lMoosel <ccanata@stevens.edu>
2024-05-06 15:00:05 -0400

    Fixed Linux script

--- start.sh
+++ start.sh
@@ -3,16 +3,8 @@
 # fuser --kill 5173/tcp # kills process running on port 5173 (hopefully a Vite server)
 lsof -i tcp:5173 | grep -v PID | awk '{print $2}' | xargs kill # To work on unix
 
-cd server
-npm i
-
 # docker compose up --force-recreate --detach --build &
 docker compose up &
 
-cd ../my-react-app
-npm i
-npm run dev &
-
-cd ..
 echo "frontend at: http://localhost:5173/"
 echo "backend at: http://localhost:4000/"
