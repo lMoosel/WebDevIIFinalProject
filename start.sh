573360a42c064c257fbb3f8efe2047189c830cb8
Charles Canata <ccanata@stevens.edu>
2024-05-07 15:06:40 -0400

    programmatically getting backend IP and creating docker-compose based off of it

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@
 # fuser --kill 5173/tcp # kills process running on port 5173 (hopefully a Vite server)
 lsof -i tcp:5173 | grep -v PID | awk '{print $2}' | xargs kill # To work on unix
 
+export BACKEND_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
 # docker compose up --force-recreate --detach --build &
 docker compose up &
 
-echo "frontend at: http://localhost:5173/"
-echo "backend at: http://localhost:4000/"
+echo "frontend at: http://${BACKEND_IP}:5173/"
+echo "backend at: http://${BACKEND_IP}:4000/"
