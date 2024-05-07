2bff3dadb4fc4c399e3f303e8815f5f1005a2f88
Egnifluro <oeng@stevens.edu>
2024-05-07 10:53:23 -0400

    Edited current song to refetch

--- my-react-app/src/components/CurrentSong.jsx
+++ my-react-app/src/components/CurrentSong.jsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import queries from '../graphQL/index.js';
-import { CookiesProvider, useCookies } from 'react-cookie';
+import { useCookies } from 'react-cookie';
 
 export function CurrentSong (props) {
     
@@ -11,20 +12,27 @@ export function CurrentSong (props) {
         return ( <h1>CURRENT SONG</h1> );
     }
 
-    const { data, loading, error } = useQuery(queries.GET_SPOTIFY_CURRENTLY_PLAYING, {
+    const { data, loading, error, refetch } = useQuery(queries.GET_SPOTIFY_CURRENTLY_PLAYING, {
         variables: {
             id: user._id
         },
         pollInterval: 30 * 1000
     });
 
-    if(!loading && false) {
-        console.log("Error: ", error)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            refetch();
+        }, 30000); // Refetch every 30 seconds
+        return () => clearInterval(interval);
+    }, [user._id]); // Refetch when user ID changes (shouldn't be necessary but just in case)
+
+    if (!loading && error) {
+        console.log("Error: ", error);
     }
 
-    if(!loading) {
-        console.log(data.getSpotifyCurrentlyPlaying)
-        console.log("user's id: ", user._id)
+    if (!loading) {
+        console.log(data.getSpotifyCurrentlyPlaying);
+        console.log("User's id: ", user._id);
     }
 
     return (
@@ -39,5 +47,5 @@ export function CurrentSong (props) {
                 <p>No song currently playing!</p>
             </>}
         </div>
-    )
+    );
 }
