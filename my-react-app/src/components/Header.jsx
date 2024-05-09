def4482b158af754ea80caab17fc4dadf1614eae
lMoosel <ccanata@stevens.edu>
2024-05-08 21:09:11 -0400

    Added Alt tag that got dropped in the merge

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -11,13 +11,16 @@ export function Header({ logout }) {
             {user && 
                 <>
                     { user.profile_picture && user.profile_picture.length ?
-                        <span>
-                            <span id="pfp-div"><img 
-                                src={ user.profile_picture[0].url }
-                                alt="Your profile picture"
-                            /></span></span> :
-                            <span><span id="pfp-div"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"></img></span></span>
-                        
+                        <span id="pfp-div"><img 
+                            src={ user.profile_picture[0].url }
+                            alt="Your profile picture"
+                        /></span> :
+                        <span id="pfp-div">
+                            <img
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"
+                                alt="Default Pfp"
+                            />
+                        </span>
                     }
                     { user.username && 
                         <span><a id="username-header"><Link to={`/user/${user._id}`}>{user.username}</Link></a></span>
@@ -40,50 +43,5 @@ export function Header({ logout }) {
             }
         </div>
     );
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
-  let user = cookies.user;
-  console.log("user: ", user);
-  return (
-    <div id="header">
-      {user && (
-        <div>
-          {user.profile_picture && user.profile_picture.length ? (
-            <div id="pfp-div">
-              <img
-                src={user.profile_picture[0].url}
-                alt="Your profile picture"
-              />
-            </div>
-          ) : (
-            <div id="pfp-div">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png"
-                alt="Default Pfp"
-              ></img>
-            </div>
-          )}
-          {user.username && (
-            <p id="username-header">
-              <Link to={`/user/${user._id}`}>{user.username}</Link>
-            </p>
-          )}
-          <span id="Login-Logout-button">
-            <button onClick={logout}>Logout</button>
-          </span>
-          <span id="Home-button">
-            <button>
-              <Link to={`/`}>Home</Link>
-            </button>
-          </span>
-          <span id="Explain-button">
-            <button>
-              <Link to={`/explain`}>What do all these stats mean?</Link>
-            </button>
-          </span>
-        </div>
-      )}
-      {!user && <button id="Login-Logout-button">Login/Signup</button>}
-    </div>
-  );
 }
 
